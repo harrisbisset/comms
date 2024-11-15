79ecd28ec4567447725367d04374fa8f3d9131a1
Harris Bisset <harrisbisset@gmail.com>
2024-11-15 13:10:49 +0000

    client working

--- udp_client/bin/main.ml
+++ udp_client/bin/main.ml
@@ -1,1 +1,8 @@
-let () = print_endline "Hello, World!"
+let () =
+  print_endline "Launching UDP Client...";
+
+  let () = Logs.set_reporter (Logs.format_reporter ()) in
+  let () = Logs.set_level (Some Logs.Info) in
+
+  let client_socket = Client.create_socket () in
+  Lwt_main.run Client.(create_client client_socket)
